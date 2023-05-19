99b8b78567c1f9eb07b418d36861bff2ea3e2b83
fbansept <bansept.franck@gmail.com>
2023-05-19 08:37:01 +0200

    jwt

--- suppression-article.php
+++ suppression-article.php
@@ -4,17 +4,23 @@ include 'header-json.php';
 
 try {
 
+    $enTetes = getallheaders();
+    $jwt = $enTetes['Authorization'];
+
     include 'connexion.php';
    
     $requete = $bdd->prepare("DELETE FROM articles WHERE id = :id");
-    $requete->execute([
-        "id" => $_GET["id"]
-    ]);
+    // $requete->execute([
+    //     "id" => $_GET["id"]
+    // ]);
 
-    echo '{ "message" : "L\'article est supprimé" }';
+    echo "{ \"message\" : \"$jwt\" }";
 
 }
 catch (PDOException $e) {
     echo '{ "message" : "Echec de la connexion : ' . $e->getMessage() . '"}';
+    exit;
+}catch (Exception $e) {
+    echo '{ "message" : "Echec : ' . $e->getMessage() . '"}';
     exit;
 }
